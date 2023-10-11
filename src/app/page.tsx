afae49333ce49377fe9a196200e4bda0d94955cd
unknown <Dell@RN-BLR-LT-18.Fugo001.net>
2023-10-11 10:22:03 +0530

    updated nextjs 13.5 build successfull

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
 
   return (
     <>
-      <h1 className='h1 font-bold text-3xl md:text-4xl'>Your feed</h1>
+      <h1 className='h1 font-bold text-3xl md:text-4xl'> Your feed</h1>
       <div className='grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6'>
         {/* @ts-expect-error server component */}
         {session ? <CustomFeed /> : <GeneralFeed />}
